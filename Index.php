c48309f5580d684339d0757a5190e51519a3c8a1
Yanis Ramssamy <ramssamyyanis@gmail.com>
2024-09-20 11:45:10 +0200

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,3 +1,7 @@
 <h1> GitHub Desktop</h1>
 
 je  te afk sur saprking zero 
+Smoky BBQ Lover
+Le Smoky BBQ Lover, un pain au blé torréfié, des tranches de bacon et de cheddar, une viande grillée à la flamme et une sauce smoky chipotle.
+Smoky BBQ
+Découvrez le Veggie Chicken Smoky BBQ : Un pain au blé torréfié, un pané végétal croustillant, des tranches de bacon végétal La Vie et de cheddar le tout accompagné d'une sauce smoky chipotle. Même chaîne opératoire que les autres produi
