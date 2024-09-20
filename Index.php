ff9bfc651882fcb24c3f5042b64501cb1036985f
Yanis Ramssamy <ramssamyyanis@gmail.com>
2024-09-20 12:44:22 +0200

    Update Index.php

--- Index.php
+++ Index.php
@@ -4,4 +4,11 @@ je ne joue pas sur saprking zero
 Smoky BBQ Lover
 Le Smoky BBQ Lover, un pain au blé torréfié, des tranches de bacon et de cheddar, une viande grillée à la flamme et une sauce smoky chipotle.
 Smoky BBQ
-Découvrez le Veggie Chicken Smoky BBQ : Un pain au blé torréfié, un pané végétal croustillant, des tranches de bacon végétal La Vie et de cheddar le tout accompagné d'une sauce smoky chipotle. Même chaîne opératoire que les autres produi
+Découvrez le Veggie Chicken Smoky BBQ : Un pain au blé torréfié, un pané végétal croustillant, des tranches de bacon végétal La Vie et de cheddar le tout accompagné d'une sauce smoky chipotle. Même chaîne opératoire que les autres produiQuoi, on vous a pas dit ? Vous êtes inscrit à la Flame
+Academia ! Dans cette école, un seul examen
+d'entrée, celui du meilleur burger.
+
+Figurez-vous que deux de vos camarades de classe
+se sont déjà bien fait remarquer : ce nerd de Deku
+et Katsuki Bakugo. Les deux ont déjà fait pas mal
+de dégâts et il n'y a que vous pour les départag
